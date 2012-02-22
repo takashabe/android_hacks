1c0cdc2236e5ff659d8731696de9964b8b4ef830
takashabe <takashabe.public@gmail.com>
2012-02-22 21:27:34 +0900

    hack7 アクティビティHacks

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -2,12 +2,44 @@ package com.android.android_hacks
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import android.content.pm._
+import android.widget._
+import android.content._
+import android.view.View._
+import android.view._
 
 class MainActivity extends Activity with TypedActivity {
+  var mButton: Button = null
+  var mIsSubActivityEnable: Boolean = false
+  var mManager: PackageManager = null
+  var mCname: ComponentName = null
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello, world!")
+    mButton = findView(TR.button01)
+    mManager = getPackageManager()
+    mCname = new ComponentName(this, SubActivity.getClass)
+    mIsSubActivityEnable = !(mManager.getComponentEnabledSetting(mCname) == PackageManager.COMPONENT_ENABLED_STATE_DISABLED)
+    setButtonText()
+    mButton.setOnClickListener(new OnClickListener() {
+      def onClick(v: View):Unit = {
+        mIsSubActivityEnable match {
+          case true => mManager.setComponentEnabledSetting(mCname, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
+          case false => mManager.setComponentEnabledSetting(mCname, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP)
+        }
+        mIsSubActivityEnable = !mIsSubActivityEnable
+        setButtonText()
+      }
+    })
   }
+
+  def setButtonText():Unit = {
+    mIsSubActivityEnable match {
+      case true => mButton.setText("Disable SubActivity")
+      case false => mButton.setText("Enable SubActivity")
+    }
+  }
+
 }
