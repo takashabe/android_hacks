882782561b7661abc76c62116db4367e26886fe3
takashabe <takashabe.public@gmail.com>
2012-03-12 02:33:20 +0900

    p86

--- src/main/scala/Hack10_MainActivity.scala
+++ src/main/scala/Hack10_MainActivity.scala
@@ -1,9 +1,9 @@
 package com.android.android_hacks
 
-import android.app.{Activity, AlertDialog, Dialog}
+import android.app.{Activity, AlertDialog, Dialog, DatePickerDialog}
 import android.os.Bundle
 import android.content.DialogInterface
-import android.widget.{Button, EditText}
+import android.widget.{Button, EditText, DatePicker}
 import android.view.View
 import android.view.View.OnClickListener
 
@@ -17,16 +17,18 @@ class Hack10_MainActivity extends TypedActivity {
     val button: Button = findView(TR.AlertDialogButtonOk)
     button.setOnClickListener(new OnClickListener() {
       override def onClick(view: View): Unit = {
-        mDialog = new Dialog(Hack10_MainActivity.this)
-        mDialog.setContentView(R.layout.dialog_ok)
-        mDialog.setTitle("Dialog with OK")
-        mDialog.findViewById(R.id.ButtonOK).setOnClickListener(new OnClickListener() {
-          override def onClick(view: View): Unit = {
-            mDialog.dismiss()
-            mResultText.setText("Dialog [OK] pressed")
-          }
-        })
-        mDialog.show
+        val datePickerDialog: DatePickerDialog =
+          new DatePickerDialog(Hack10_MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            def onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int): Unit =
+              mResultText.setText("%04d/%02d/%02d".format(year, monthOfYear+1, dayOfMonth))
+          }, 2012, 3, 12)
+        datePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            override def onClick(dialog: DialogInterface, which: Int): Unit = mResultText.setText("DatePickerDialog Canceled.")
+          })
+        datePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            override def onClick(dialog: DialogInterface, which: Int): Unit = mResultText.setText("DatePickerDialog OK.")
+          })
+        datePickerDialog.show()
       }
     })
     mResultText = findView(TR.textResult)
