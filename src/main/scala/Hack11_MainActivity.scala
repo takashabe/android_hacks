a4a90cb0b083bf1dd3817a972f51ec105ad5740e
takashabe <takashabe.public@gmail.com>
2012-03-18 18:28:29 +0900

    hack11

--- src/main/scala/Hack11_MainActivity.scala
+++ src/main/scala/Hack11_MainActivity.scala
@@ -10,18 +10,18 @@ import android.content.{SharedPreferences, Context}
 class Hack11_MainActivity extends TypedActivity {
   private var pref1 = 100
   private var pref2 = "hoge"
-  lazy val sp:SharedPreferences = getSharedPreferences("savePreferences", Context.MODE_PRIVATE)
 
   override def onCreate(savedInstanceBundle: Bundle) {
     super.onCreate(savedInstanceBundle)
     setContentView(R.layout.hack11_main)
 
     val textView = findView(TR.textView)
+    val sp: SharedPreferences = getSharedPreferences("savePreferences", Context.MODE_PRIVATE)
 
     val saveButton = findView(TR.saveButton)
     saveButton.setOnClickListener(new OnClickListener() {
       override def onClick(view: View): Unit = {
-        putPreference()
+        putPreference(sp)
         textView.setText("saved Prefences")
       }
     })
@@ -29,29 +29,25 @@ class Hack11_MainActivity extends TypedActivity {
     val loadButton = findView(TR.loadButton)
     loadButton.setOnClickListener(new OnClickListener() {
       override def onClick(view: View): Unit = {
-        // getPreference()
-        textView.setText("load Prefences" + getPreference())
+        textView.setText("load Prefences: " + getPreference(sp))
       }
     })
   }
 
 
-
-  def putPreference(): Unit = {
-    // sp = getSharedPreferences("key", Context.MODE_PRIVATE)
+  def putPreference(sp: SharedPreferences): Unit = {
     val editor: SharedPreferences.Editor = sp.edit()
     editor.putInt("id", pref1)
     editor.putString("name", pref2)
     editor.commit()
   }
 
-  def getPreference(): String = {
-    // sp = getSharedPreferences("key", Context.MODE_PRIVATE)
+  def getPreference(sp: SharedPreferences): String = {
     sp.getInt("id", -1)
     sp.getString("name", "None")
   }
 
-  def rmPreference(): Unit = {
+  def rmPreference(sp: SharedPreferences): Unit = {
     val editor: SharedPreferences.Editor = sp.edit()
     editor.remove("id")
     editor.remove("name")
